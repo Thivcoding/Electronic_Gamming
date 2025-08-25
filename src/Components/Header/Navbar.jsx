a5bde6b4f5e7501c46e972634c4492deb6c8ed90
Thivcoding <vantiv21@gmail.com>
2025-08-25 14:26:50 +0700

    first commit

---
+++ src/Components/Header/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className='w-full h-[200px] bg-gray-500'>
+        {/* top  */}
+        <h1 className='w-full flex justify-center items-center text-white
+         font-bold text-xs h-[20%] bg-red-500'>
+            ENJOY FAST & FREE SHIPPING STOREWIDE!
+        </h1>
+    </header>
+  )
+}
+
+export default Navbar
---
+++ src/Data.js
@@ -0,0 +1,18 @@
+export const menu_top = [
+    {  name: "Home", link: "/" },
+    {  name: "Shop", link: "/shop" },
+    {  name: "Pages", link: "/pages" },
+    {  name: "Blog", link: "/blog" },
+];
+
+export const menu_bottom = [
+  { name: "HEADSETS", link: "/headsets" },
+  { name: "KEYBOARDS", link: "/keyboards" },
+  { name: "MOUSE", link: "/mouse" },
+  { name: "GAMING CONTROLLERS", link: "/gaming-controllers" },
+  { name: "MONITORS", link: "/monitors" },
+  { name: "MICROPHONES", link: "/microphones" },
+  { name: "POWER", link: "/power" },
+  { name: "ACCESSORIES", link: "/accessories" },
+  { name: "SALE", link: "/sale" }
+];
