a979869718d763368e2e1889981f424457bb3948
Thivcoding <vantiv21@gmail.com>
2025-08-27 15:15:50 +0700

    Already Navbar

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -6,30 +6,44 @@ import { FaRegUser } from "react-icons/fa";
 import { FaRegHeart } from "react-icons/fa";
 import { FiShoppingCart } from "react-icons/fi";
 import { IoCloseSharp } from "react-icons/io5";
+import { RiShoppingBag4Line } from "react-icons/ri";
+import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
     const [OpenSearch,setOperSearch] = useState(false);
+    const [OpenCart,setOpenCart] = useState(false);
+    const [OpenSidbar,setOpenSidbar] = useState(false);
+
   return (
-    <header className='w-full shadow-md h-[200px] bg-gray-500'>
+    <header className='w-full shadow-md h-[100px] lg:h-[200px] bg-gray-500'>
       
         {/* top  */}
-        <h1 className='w-full flex justify-center items-center text-white
-         font-bold text-xs h-[20%] bg-red-500'>
+        <h1 className='w-full flex justify-center  items-center text-white
+         font-bold text-xs h-10 lg:h-[20%] bg-red-500'>
             ENJOY FAST & FREE SHIPPING STOREWIDE!
         </h1>
 
         {/* nav-top */}
-        <nav className='w-full h-[50%] flex px-14 bg-black'>
+        <nav className='w-full h-[80%] lg:h-[50%] flex justify-between items-center px-5 lg:px-14 bg-black'>
+           
+            <div className='flex lg:hidden '> 
+              {/* icon bar  */}
+              <FaBars className=' text-white text-3xl mr-3' onClick={()=>setOpenSidbar(!OpenSidbar)} />
+
+              {/* icon search */}
+              <IoMdSearch className=' text-white text-4xl ' onClick={()=>setOperSearch(!OpenSearch)} />
+            </div>
+          
             {/* logo */}
-            <a className='w-[15%] h-full flex items-center' href="">
+            <a className='lg:w-[15%] h-full flex items-center' href="">
               <img src={logo} alt="logo" />
             </a>
 
             {/* nav-right */}
-            <div className='w-[85%] flex h-full'>
+            <div className='lg:w-[85%] flex h-full'>
 
                 {/* menu */}
-                <ul className='w-[40%] h-full flex items-center justify-evenly'>
+                <ul className='lg:w-[40%] h-full hidden lg:flex items-center justify-evenly'>
                   {menu_top.map((e)=>(
                     <li key={e.name}>
                         <a href={e.link} className='text-white hover:text-red-500 font-bold text-xl'>
@@ -40,7 +54,7 @@ const Navbar = () => {
                 </ul>
 
                 {/* Input Search */}
-                <div className='w-[40%] h-full py-6 relative'>
+                <div className='lg:w-[40%] hidden lg:flex h-full py-6 relative'>
                   <input type="text" className='w-full h-full border-0 outline-0
                    text-gray-500 bg-white rounded-sm px-6 text-sm' 
                     placeholder='Search Product Or Brand'
@@ -51,13 +65,15 @@ const Navbar = () => {
                 </div>
                 
                 {/* Icon  */}
-                <div className='w-[20%] flex items-center justify-around h-full'>
-                    <h1 className='text-white'>English</h1>
+                <div className='lg:w-[20%] flex items-center lg:gap-0 gap-3 justify-around h-full'>
+                    <h1 className='text-white hidden lg:flex'>English</h1>
                     <FaRegUser className='text-2xl cursor-pointer text-white' />
-                    <FaRegHeart className='text-2xl cursor-pointer text-white' />
+                    <FaRegHeart className='text-2xl hidden lg:flex  cursor-pointer text-white' />
 
                     <div className='relative '>
-                        <FiShoppingCart className='text-2xl cursor-pointer text-white ' />
+                        <FiShoppingCart 
+                        onClick={()=>setOpenCart(!OpenCart)}
+                        className='text-2xl cursor-pointer text-white ' />
                         <div className='w-5 h-5 flex items-center justify-center
                         text-white bg-red-500 cursor-pointer rounded-full absolute -right-3 -top-3'>
                           1
@@ -72,7 +88,7 @@ const Navbar = () => {
 
         {/* Category */}
         <nav className='w-full h-[30%] bg-white'>
-              <ul className='w-[80%] flex justify-around items-center m-auto h-full '>
+              <ul className='w-[80%] hidden lg:flex justify-around items-center m-auto h-full '>
                   {menu_bottom.map((e)=>(
                     <li key={e.name}>
                       <a href={e.link} className='text-sm font-bold' >{e.name}</a>
@@ -82,12 +98,12 @@ const Navbar = () => {
         </nav>
 
         {/* OpenSearch */}
-        <div className={`w-[400px] h-screen fixed top-0 left-0 
-        ${OpenSearch ? "translate-x-0" : "-translate-x-full" }
-          transition-all duration-500 ease-in-out bg-sky-600`}>
+        <div className={`w-[400px] h-screen bg-white shadow-2xl fixed top-0 lg:left-0 right-0 
+        ${OpenSearch ? "translate-x-0" : "lg:-translate-x-full translate-x-full" }
+          transition-all duration-500 ease-in-out `}>
 
           {/* inputSearch */}
-          <div className='w-full h-24 bg-white border-b relative'>
+          <div className='w-full h-24  border-b relative'>
               {/* icon search */}
                   <IoMdSearch className='absolute text-black top-9 text-3xl left-5' />
                   <input type="text" className='w-full px-16 h-full border-0 outline-0' 
@@ -97,6 +113,38 @@ const Navbar = () => {
                   />
           </div>
         </div>
+
+        {/* OpenCart */}
+            <div className={`lg:w-[500px] w-full h-screen bg-white shadow-2xl fixed top-0 right-0 
+                ${OpenCart ? "translate-x-0" : "translate-x-full" }
+                  transition-all duration-500 ease-in-out `}>
+
+                  <div className='w-full h-16  border-b relative'>
+                          <IoCloseSharp className='absolute text-black cursor-pointer top-4 text-3xl right-5'
+                            onClick={()=>setOpenCart(false)}
+                          />
+                          <div className='w-full h-full px-5 flex items-center '>
+                              <RiShoppingBag4Line className='text-3xl me-2'  />
+                            <h1 className='font-bold text-xl'>1 ITEM</h1>
+                          </div>
+                  </div>
+            </div>
+
+        {/* OpenSidbar */}
+            <div className={`lg:w-[500px] w-full h-screen bg-white shadow-2xl fixed top-0 left-0 
+                ${OpenSidbar ? "translate-x-0" : "-translate-x-full" }
+                  transition-all duration-500 ease-in-out `}>
+
+                  <div className='w-full h-16  border-b relative'>
+                          <IoCloseSharp className='absolute text-black cursor-pointer top-4 text-3xl right-5'
+                            onClick={()=>setOpenSidbar(false)}
+                          />
+                          <div className='w-full h-full px-5 flex items-center '>
+                              <RiShoppingBag4Line className='text-3xl me-2'  />
+                            <h1 className='font-bold text-xl'>ETEC CENTER</h1>
+                          </div>
+                  </div>
+            </div>
 
     </header>
   )
